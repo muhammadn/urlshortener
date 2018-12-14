af6310b76056937bef3163fb997d5bab873e6790
Muhammad Nuzaihan <muhammad@taqisystems.com>
2018-12-15 06:06:32 +0800

    improve model tests

--- spec/models/url_spec.rb
+++ spec/models/url_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Url, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is not valid without url input' do
+    url = Url.new(url: nil)
+    expect(url).to_not be_valid
+  end
+
+  it 'is not valid when it is not a URL' do
+    url = Url.new(url: 'file://test')
+    expect(url).to_not be_valid
+  end
+
+  it 'is valid when it is a correct URL' do
+    url = Url.new(url: 'http://unrealasia.net')
+    expect(url).to be_valid
+  end
 end
